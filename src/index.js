0b79df6bbeea1939d78b377f2a2a99c462313af6
shwetha <shwetha.kumari@robosoftin.com>
2021-02-16 11:22:53 +0530

    feat(random): get random starwars names
    BREAKING CHANGE: closes #2
    
    fix #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-var starWarsName = require("./starwars-names.json");
 var uniqueRandomArray = require("unique-random-array");
+var starWarsNames = require("./starwars-names.json");
+var getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
-  all: starWarsName,
-  random: uniqueRandomArray(starWarsName),
+  all: starWarsNames,
+  random: random,
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
